8701dd1b616fa0899b131841e92b3f3181688ed9
wangdunwei <wangdunwei@wxbtc.com>
2018-10-08 17:41:41 +0800

    1.添加欢迎页 2.修改gradle配置

---
+++ app/src/main/java/com/ayvytr/gank/SplachActivity.java
@@ -0,0 +1,58 @@
+package com.ayvytr.gank;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ayvytr.mvp.BaseMvpActivity;
+import com.ayvytr.mvp.IPresenter;
+import com.ayvytr.mvp.RxLifecycleUtils;
+import com.ayvytr.mvp.RxSchedulers;
+import com.trello.rxlifecycle2.android.ActivityEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class SplachActivity extends BaseMvpActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected IPresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    public void initExtra() {
+
+    }
+
+    @Override
+    public void initView(@Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+        Observable.timer(1, TimeUnit.SECONDS)
+                  .compose(RxSchedulers.<Long>applySchedulers(null))
+                  .compose(RxLifecycleUtils.<Long>bindUntilEvent(this, ActivityEvent.STOP))
+                  .subscribe(new Consumer<Long>() {
+                      @Override
+                      public void accept(Long aLong) {
+                          startActivity(new Intent(getContext(), MainActivity.class));
+                          finish();
+                      }
+                  });
+    }
+
+    @Override
+    public int getContentViewRes() {
+        return 0;
+    }
+}
